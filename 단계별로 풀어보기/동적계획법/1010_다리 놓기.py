3c90d31477f4110fd316a1682aec2e89cd384e99
Geniusin <64780996+Geniusin@users.noreply.github.com>
2023-06-02 16:25:25 +0900

    commit

---
+++ 단계별로 풀어보기/동적계획법/1010_다리 놓기.py
@@ -0,0 +1,15 @@
+x = [-1 for _ in range(6)]
+
+
+def bridge(n, m):
+
+    if n == m: return 1
+    if n == 1: return m
+
+    if x[n] != -1:
+        return x[n]
+
+    x[n] = bridge(1, m-n+1) + bridge(n-1, m-1)
+    return x[n]
+
+print(bridge(2, 5))
