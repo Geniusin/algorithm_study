e433484800d9ff04455267db36d8bcaaef2c6d84
Geniusin <64780996+Geniusin@users.noreply.github.com>
2023-11-20 17:46:41 +0900

    Create 2941_크로아티아 알파벳.py

---
+++ 단계별로 풀어보기/기타/2941_크로아티아 알파벳.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+input_str = input()
+
+
+alpas = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=',
+         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k',
+         'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+         'w', 'x', 'y', 'z']
+
+
+
